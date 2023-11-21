1a1def66cf40af9e2b7a89c44c8e184bbd48e49b
Joel Joseph <joeljordanjoseph@gmail.com>
2023-11-21 12:05:36 -0700

    Added organizations to club models. Club view will show parent organization if one exists.

---
+++ Olympsis/Data/Models/OrganizationModels.swift
@@ -0,0 +1,8 @@
+//
+//  OrganizationModels.swift
+//  Olympsis
+//
+//  Created by Joel on 11/21/23.
+//
+
+import Foundation
---
+++ Olympsis/Views/Viewers/Club/ClubTagView.swift
@@ -0,0 +1,18 @@
+//
+//  ClubTagView.swift
+//  Olympsis
+//
+//  Created by Joel on 11/19/23.
+//
+
+import SwiftUI
+
+struct ClubTagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClubTagView()
+}
