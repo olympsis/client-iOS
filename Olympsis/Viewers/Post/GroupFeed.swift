5e600d19c918269ad9319b744756eb549ce5d595
Joel Joseph <joeljordanjoseph@gmail.com>
2023-12-30 18:20:29 -0700

    post can be pinned. Org posts now show up in clubs.

---
+++ Olympsis/Viewers/Post/GroupFeed.swift
@@ -0,0 +1,18 @@
+//
+//  GroupFeed.swift
+//  Olympsis
+//
+//  Created by Joel on 12/29/23.
+//
+
+import SwiftUI
+
+struct GroupFeed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupFeed()
+}
