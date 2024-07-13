ef30958d89ca91df1efa50416441679a365e7155
josephjoeljo <joeljordanjoseph@gmail.com>
2024-07-13 09:35:03 -0600

    clipping profile picture into circle

--- Olympsis/Views/Profile/ProfileModel.swift
+++ Olympsis/Views/Profile/ProfileModel.swift
@@ -59,6 +59,7 @@ struct ProfileModel: View {
                         .resizable()
                         .setProcessor(DownsamplingImageProcessor(size: CGSize(width: 200, height: 200)))
                         .frame(width: 100, height: 100)
+                        .clipShape(Circle())
                 } else {
                     Circle()
                         .foregroundStyle(Color.background)
