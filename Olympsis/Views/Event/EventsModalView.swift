6da559e1741c5b64fd3cf007917becd1117846a9
josephjoeljo <joeljordanjoseph@gmail.com>
2024-07-20 10:35:09 -0600

    sorted events in their respecitve day groups

--- Olympsis/Views/Event/EventsModalView.swift
+++ Olympsis/Views/Event/EventsModalView.swift
@@ -50,15 +50,24 @@ struct EventsModalView: View {
                 return
             }
         }
-        return groups.sorted { (group1: DayGroup, group2: DayGroup) in
-            if areDatesOnSameDay(date1: Date(timeIntervalSince1970: TimeInterval(group1.timestamp)), date2: Date(timeIntervalSince1970: TimeInterval(group2.timestamp))) {
-                // If dates are on the same day, prioritize item1
-                return true
-            } else {
-                // If dates are not on the same day, sort by timestamp
-                return group1.timestamp < group2.timestamp
+        
+        var sorted = groups
+            .sorted { (group1: DayGroup, group2: DayGroup) in
+                if areDatesOnSameDay(date1: Date(timeIntervalSince1970: TimeInterval(group1.timestamp)), date2: Date(timeIntervalSince1970: TimeInterval(group2.timestamp))) {
+                    // If dates are on the same day, prioritize item1
+                    return true
+                } else {
+                    // If dates are not on the same day, sort by timestamp
+                    return group1.timestamp < group2.timestamp
+                }
+            }
+        for i in 0..<sorted.count {
+            sorted[i].events = sorted[i].events.sorted { event1, event2 in
+                return event1.startTime ?? 0 < event2.startTime ?? 0
             }
         }
+        
+        return sorted
     }
     
     var body: some View {
