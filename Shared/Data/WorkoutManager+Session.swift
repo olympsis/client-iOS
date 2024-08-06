741b1685c214f1f68af645b88c154ec308b83d51
josephjoeljo <joeljordanjoseph@gmail.com>
2024-08-06 07:50:01 -0600

    wip: trying to get location working

--- Shared/Data/WorkoutManager+Session.swift
+++ Shared/Data/WorkoutManager+Session.swift
@@ -120,6 +120,57 @@ extension WorkoutManager {
         heartRate = 0
         distance = 0
     }
+    
+    func listenToLocationUpdates() {
+        Task {
+            guard let routeBuilder else {
+                self.log.info("Route builder not created")
+                return
+            }
+            
+            self.manager.requestWhenInUseAuthorization()
+            self.manager.startUpdatingLocation()
+            
+            if #available(iOS 18.0, watchOS 11.0, *) {
+                let session = CLServiceSession(authorization: .whenInUse, fullAccuracyPurposeKey: "fitness")
+                
+                for try await diagnostic in session.diagnostics {
+                    if diagnostic.authorizationDenied {
+                        self.log.info("Authorization denied")
+                    } else if diagnostic.authorizationRestricted {
+                        self.log.info("Authorization restricted")
+                    } else if diagnostic.authorizationDeniedGlobally {
+                        self.log.info("Authorization denied globally")
+                    } else if diagnostic.authorizationRequestInProgress {
+                        self.log.info("Authorization request in progress")
+                    } else if diagnostic.fullAccuracyDenied {
+                        self.log.info("Full accuracy denied")
+                    } else if diagnostic.insufficientlyInUse {
+                        self.log.info("Insufficiently in use")
+                    } else if diagnostic.serviceSessionRequired {
+                        self.log.info("Service session required")
+                    }
+                }
+            }
+            
+            self.backgroundActivity = CLBackgroundActivitySession()
+            let updates = CLLocationUpdate.liveUpdates(.fitness)
+            for try await update in updates {
+                guard let loc = update.location,
+                      loc.horizontalAccuracy <= 50.0 else {
+                    self.log.info("Location accuracy too low or location not recieved.")
+                    return
+                }
+                try await routeBuilder.insertRouteData([loc])
+                self.log.info("Route Location added: \(loc.coordinate.latitude), \(loc.coordinate.longitude)")
+            }
+        }
+    }
+    
+    func stopListeningToLocationUpdates() {
+        backgroundActivity?.invalidate()
+        backgroundActivity = nil
+    }
 }
 
 #if os(watchOS)
