547d9537f242561d47c8d170bc02fca62e6f1b70
josephjoeljo <joeljordanjoseph@gmail.com>
2025-03-30 17:11:39 -0400

    created a new filter button

---
+++ Olympsis/Views/Buttons/FilterButton.swift
@@ -0,0 +1,44 @@
+//
+//  FilterButton.swift
+//  Olympsis
+//
+//  Created by Joel Joseph on 3/30/25.
+//
+
+import SwiftUI
+
+struct FilterButton: View {
+    
+    @Binding var numActive: Int
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack(spacing: 5) {
+                if (numActive > 0) {
+                    Text("\(numActive)")
+                        .fontWeight(.bold)
+                        .padding(.vertical, 2.5)
+                        .padding(.horizontal, 7)
+                        .background(Color.blue.opacity(0.3))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                } else {
+                    Image(systemName: "line.3.horizontal.decrease")
+                        .imageScale(.small)
+                        .fontWeight(.medium)
+                }
+                
+                Text("Filters")
+                    .fontWeight(.medium)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+#Preview {
+    FilterButton(numActive: .constant(0), action: {})
+}
