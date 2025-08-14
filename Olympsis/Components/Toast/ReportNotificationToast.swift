90dc2cf453acfe14bb51212942a0226a4b187279
Joel Joseph <joeljordanjoseph@gmail.com>
2025-08-14 10:20:27 -0700

    post notification works great

---
+++ Olympsis/Components/Toast/ReportNotificationToast.swift
@@ -0,0 +1,18 @@
+//
+//  ReportNotificationToast.swift
+//  Olympsis
+//
+//  Created by Joel Joseph on 8/13/25.
+//
+
+import SwiftUI
+
+struct ReportNotificationToast: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportNotificationToast()
+}
