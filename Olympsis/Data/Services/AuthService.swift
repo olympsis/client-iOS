85145d54b09cfc32d8ba1d59d76e0a09d7b2421a
Joel Joseph <joeljordanjoseph@gmail.com>
2026-04-05 19:44:04 -0400

    forgot to pass  in headers

--- Olympsis/Data/Services/AuthService.swift
+++ Olympsis/Data/Services/AuthService.swift
@@ -31,7 +31,7 @@ class AuthService {
     func modify(request: AuthUserDao) async throws -> (Data, URLResponse){
         let headers = try await AppEnvironment.authHeaders()
         let endpoint = Endpoint("/v1/auth/modify")
-        return try await http.Request(.PUT, endpoint, body: EncodeToData(request))
+        return try await http.Request(.PUT, endpoint, body: EncodeToData(request), headers: headers)
     }
     
     func deleteAccount() async throws -> (Data, URLResponse){
